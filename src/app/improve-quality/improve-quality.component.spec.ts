6fc2b4a49401d7cc03d402d65e20e225d96829c6
Vibek-D <vikiisbck@gmail.com>
2021-06-16 16:32:15 +0530

    layout done

--- src/app/improve-quality/improve-quality.component.spec.ts
+++ src/app/improve-quality/improve-quality.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ImproveQualityComponent } from './improve-quality.component';
+
 
 describe('ImproveQualityComponent', () => {
   let component: ImproveQualityComponent;
--- src/app/improve-quality/improve-quality.component.ts
+++ src/app/improve-quality/improve-quality.component.ts
@@ -5,25 +5,27 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './improve-quality.component.html',
   styleUrls: ['./improve-quality.component.css']
 })
+
 export class ImproveQualityComponent implements OnInit {
 
   constructor() { }
 
+  changingText: string = 'Quality';
+
   ngOnInit(): void {
+    setTimeout(() => {
+      this.changingText = 'Resolution';
+    }, 1000);
   }
 
   onFileSelected() {
     const inputNode: any = document.querySelector('#file');
-  
     if (typeof (FileReader) !== 'undefined') {
       const reader = new FileReader();
-  
       reader.onload = (e: any) => {
         // this.srcResult = e.target.result;
       };
-  
       reader.readAsArrayBuffer(inputNode.files[0]);
     }
   }
-
 }
